6f3ea99108d1b275d2823fac7b2e3d49df4d3121
Sourav Mondal <souravrkl.gamefreak@gmail.com>
2017-02-19 20:45:16 +0530

    [ADD] search functionality

--- src/app/movie/movie.controller.js
+++ src/app/movie/movie.controller.js
@@ -9,26 +9,12 @@
     function MovieController(Movies, $log) {
         var vm = this;
         vm.moviesList = null;
-        vm.movie = {
-            "movie_title": "Avatar ",
-            "director_name": "James Cameron",
-            "actor_1_name": "CCH Pounder",
-            "actor_2_name": "Joel David Moore",
-            "genres": "Action|Adventure|Fantasy|Sci-Fi",
-            "language": "English",
-            "country": "USA",
-            "content_rating": "PG-13",
-            "budget": "237000000",
-            "title_year": "2009",
-            "plot_keywords": "avatar|future|marine|native|paraplegic",
-            "movie_imdb_link": "http://www.imdb.com/title/tt0499549/?ref_=fn_tt_tt_1"
-        };
         vm.currentPage = 1;
         vm.totalMovies = 0;
         vm.movieStartIndex = 0;
+        vm.searchText = '';
 
         vm.pageChange = pageChange;
-
 
         if(localStorage.getItem('stored') === true) {
             Movies.setMovies(angular.fromJson(localStorage.getItem('movieList')));
@@ -50,7 +36,6 @@
         }
 
         function pageChange() {
-            $log.info(vm.currentPage);
             vm.movieStartIndex = (vm.currentPage - 1) * 10;
         }
     }
